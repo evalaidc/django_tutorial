18ce5409f10a621e2f6bec66b43fd766a891f153
Eva Lai <evalai.dc@gmail.com>
2017-05-31 14:29:24 -0400

    created new views for polls

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -9,4 +9,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("question %s." % question_id)
+
+def results(request, question_id):
+    response = "Results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Votes of question %s." % question_id)
